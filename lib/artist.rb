7d5a5aaca673cedaedd3c55d95c3212091489619
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-08-13 00:46:32 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,17 +3,21 @@ require_relative '../lib/concerns/memorable'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  extend Memorable
+  extend Memorable::ClassMethods
+  extend Findable
+  include Paramable
+  include Memorable::InstanceMethods
+
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
@@ -36,7 +40,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -3,16 +3,21 @@ require_relative '../lib/concerns/memorable'
 class Song
   attr_accessor :name
   attr_reader :artist
-  extend Memorable
+  extend Memorable::ClassMethods
+  extend Findable
+  include Paramable
+  include Memorable::InstanceMethods
+
   @@songs = []
 
   def initialize
-    @@songs << self
+    super
+    @songs = []
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
@@ -30,7 +35,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
